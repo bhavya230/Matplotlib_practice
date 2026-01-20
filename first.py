704a0201900f9c59163b84cfef652944167473bc
unknown <raghavbhavyasingh@gmail.com>
2026-01-20 10:44:59 +0530

    importing pyplot

--- first.py
+++ first.py
@@ -1,5 +1,9 @@
 import matplotlib
 import numpy as np
 
+# importing pyplot submodule of matplotlib-- that contains most of its utilities 
+
+import matplotlib.pyplot as plt
+
 # checking version of matplotlib
 print(matplotlib.__version__)
