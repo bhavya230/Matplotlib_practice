478234c98ef9fdbe50ae28ae028de4ca4ba8c6d6
unknown <raghavbhavyasingh@gmail.com>
2026-01-20 14:00:47 +0530

    sactter plot

--- first.py
+++ first.py
@@ -14,3 +14,14 @@ y=np.arange(11,21)
 
 a=np.arange(40,50)
 b=np.arange(50,60)
+
+# plotting using matplotlib
+
+# plt scatter plot
+
+plt.scatter(x,y,c="g")
+plt.xlabel('x axis') # lael for x axis 
+plt.ylabel('y axis') # label for y axis
+plt.title('Graph in 2D') # gives title to graph 
+plt.show() # show the graph
+plt.savefig("Test.png") # to save the image directly 
